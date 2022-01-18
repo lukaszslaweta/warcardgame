b115c8fad15775846c07f5d3bc20787f34c933e4
lukaszslaweta <lukasz.slaweta@gmail.com>
2022-01-18 13:12:35 +0100

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Łukasz Sławęta on 18/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
